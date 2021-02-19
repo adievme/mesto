ce01131cfed323ec8ea982f1d49d18ecc73d7ada
Insaf Adiev <adievme99@yandex.ru>
2021-02-19 09:12:00 +0300

    project

---
+++ scripts/index.js
@@ -0,0 +1,25 @@
+let openButton = document.querySelector('.profile__edit-button');
+let popup = document.querySelector('.popup');
+let closeButton = document.querySelector('.popup__close');
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('#input_name');
+let jobInput = document.querySelector('#input_job');
+let nameProfile = document.querySelector('.profile__name');
+let jobProfile = document.querySelector('.profile__job');
+// открытие и закрытие Popup
+function openPopup() {
+    popup.classList.toggle('popup__active')
+}
+nameInput.value = nameProfile.textContent; //Загрузка информации из страницы в поля ввода попапа
+jobInput.value = jobProfile.textContent;
+openButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', openPopup);
+//Сохранение изменений в поле ввода
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    nameProfile.textContent = nameInput.value;
+    jobProfile.textContent = jobInput.value;
+}       
+formElement.addEventListener('submit', formSubmitHandler); 
+formElement.addEventListener('submit', openPopup); //закрытие попап при нажатии на "сохранить"
+
