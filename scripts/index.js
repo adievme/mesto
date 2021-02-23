c9cf96d95c732b63716414924b20823c7bd373bc
Insaf Adiev <adievme99@yandex.ru>
2021-02-23 15:56:36 +0300

    update

--- scripts/index.js
+++ scripts/index.js
@@ -6,7 +6,7 @@ let nameInput = document.querySelector('#input_name');
 let jobInput = document.querySelector('#input_job');
 let nameProfile = document.querySelector('.profile__name');
 let jobProfile = document.querySelector('.profile__job');
-// открытие и закрытие Popup
+
 function openPopup() {
     if (popup.classList.contains('popup')) {
         nameInput.value = nameProfile.textContent;
@@ -15,14 +15,13 @@ function openPopup() {
     }
 }
 
-openButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', openPopup);
-//Сохранение изменений в поле ввода
 function formSubmitHandler (evt) {
     evt.preventDefault();
     nameProfile.textContent = nameInput.value;
     jobProfile.textContent = jobInput.value;
+    openPopup();
 }       
 formElement.addEventListener('submit', formSubmitHandler); 
-formElement.addEventListener('submit', openPopup); //закрытие попап при нажатии на "сохранить"
+openButton.addEventListener('click', openPopup);
+closeButton.addEventListener('click', openPopup);
 
