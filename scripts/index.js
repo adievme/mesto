d4dfedcc44c91dd1f3de308a4912e1e1567bb876
Insaf Adiev <adievme99@yandex.ru>
2021-03-07 08:22:23 +0300

    feat: добавить открытие и закрытие попапов

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,63 @@
-let openButton = document.querySelector('.profile__edit-button');
-let popup = document.querySelector('.popup');
-let closeButton = document.querySelector('.popup__close');
-let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('#input_name');
-let jobInput = document.querySelector('#input_job');
-let nameProfile = document.querySelector('.profile__name');
-let jobProfile = document.querySelector('.profile__job');
+const editButton = document.querySelector('.profile__edit-button');
+      addButton = document.querySelector('.profile__add-button');
+      nameProfile = document.querySelector('.profile__name');
+      jobProfile = document.querySelector('.profile__job');
+      popup = document.querySelector('.popup');
 
-function openPopup() {
-    if (popup.classList.contains('popup')) {
+      //likeCard = document.querySelectorAll('.elements__like-button');
+      
+      editFormModalWindow = document.querySelector('.edit-profile');
+      cardFormModalWindow = document.querySelector('.add-card');
+
+      closeButton = document.querySelectorAll('.popup__close');
+      formElement = document.querySelector('.popup__form');
+
+      nameInput = document.querySelector('#input_name');
+      jobInput = document.querySelector('#input_job');
+
+let modalWindow;
+
+//Открыть попап
+function openModalWindow(modalWindow) {
+    modalWindow.classList.add('popup_active');
+}
+//Закрыть попап
+function closeModalWindow(event) {
+    event.target.closest('.popup').classList.remove('popup_active');
+}
+//Показать попап редактирования профиля 
+function openPopupEditProfile() {
+    openModalWindow(editFormModalWindow);
+    if (popup.classList.contains('popup') ) {
         nameInput.value = nameProfile.textContent;
         jobInput.value = jobProfile.textContent;
-        popup.classList.toggle('popup_active');
     }
 }
+//Показать попап добавления нового места
+function openPopupAddCard() {
+    openModalWindow(cardFormModalWindow);
+}
+/*
+function like(evt) {
+    likeCard.addEventListener('click', evt.target.classList.toggle('elements__like-button_active'));
+}
+*/
+closeButton.forEach((item) => {
+    item.addEventListener('click', closeModalWindow);
+});
 
-function formSubmitHandler (evt) {
+function formSubmitHandler(evt) {
     evt.preventDefault();
     nameProfile.textContent = nameInput.value;
     jobProfile.textContent = jobInput.value;
-    openPopup();
-}       
+    closeModalWindow(evt);
+}
+
+editButton.addEventListener('click', openPopupEditProfile);
+addButton.addEventListener('click', openPopupAddCard);
 formElement.addEventListener('submit', formSubmitHandler); 
-openButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', openPopup);
+
+
+
+
 
